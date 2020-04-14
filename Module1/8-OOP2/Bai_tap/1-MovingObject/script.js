3f26cb3f039f341d64a5d7023a91d2a52d769be5
NGuyen HOang DUy <nguyenhoang.duy154@gmail.com>
2020-04-14 11:11:14 +0700

    add codes

---
+++ Module1/8-OOP2/Bai_tap/1-MovingObject/script.js
@@ -0,0 +1,66 @@
+/**
+ * Created by nhatnk on 4/26/17.
+ */
+
+function Hero(image, top, left, size, speed){
+  this.image = image;
+  this.top = top;
+  this.left = left;
+  this.size = size;
+  this.speed = speed;
+  this.getHeroElement = function(){
+    return '<img width="'+ this.size + '"' +
+      ' height="'+ this.size + '"' +
+      ' src="' + this.image +'"' +
+      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+  }
+
+  this.moveRight = function(){
+    this.left += speed;
+    // console.log('ok: ' + this.left);
+  }
+  this.moveLeft = function(){
+    this.left -= speed;
+    // console.log('ok: ' + this.left);
+  }
+  this.moveDown = function(){
+    this.top += speed;
+    // console.log('ok: ' + this.left);
+  }
+  this.moveUp = function(){
+    this.top -= speed;
+    // console.log('ok: ' + this.left);
+  }
+
+}
+
+var hero = new Hero('ninja.jpg', 30, 20, 100, 30);
+let status = "right";
+function start(){
+  switch(status) {
+      case "right":
+          if(hero.left < window.innerWidth - hero.size){
+              hero.moveRight();
+          } else status = "down";
+          break;
+      case "down":
+          if(hero.top < window.innerHeight - hero.size){
+              hero.moveDown();
+          } else status = "left";
+          break;
+      case "left":
+          if(hero.left >= 20){
+              hero.moveLeft();
+          } else status = "up";
+          break;
+      case "up":
+          if(hero.top >= 30){
+              hero.moveUp();
+          } else status = "right";
+          break;
+  }
+  document.getElementById('game').innerHTML = hero.getHeroElement();
+  setTimeout(start, 100)
+}
+start();
+        
